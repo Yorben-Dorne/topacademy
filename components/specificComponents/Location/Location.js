bc89f36c66c5c4304e1f99e9405ccbc9eed08a9e
Lars <lars.matthys@hotmail.be>
2022-12-15 09:40:42 +0100

    okee

--- components/specificComponents/Location/Location.js
+++ components/specificComponents/Location/Location.js
@@ -35,6 +35,7 @@ export default class Location extends Component {
 				<main>
 					<Hero blok={this.props.blok} contentTypeTag="course" />
 					<div className={css["location__main-content"]}>
+						hallo
 						<div id="location__short-description" key="location__short-description" className={css["location__short-description"]}>
 							<section className={css["rich-text-section--with-navigator"]}>
 								<h2 className={css["rich-text-section__title"]}>Hello everyone</h2>
